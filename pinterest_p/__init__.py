8499b574c077ade0388f4a583a35cf02d8c74f44
BraulioHermanson <brauliohcampos0@outlook.com>
2023-09-18 11:34:11 -0300

    commit

--- pinterest_p/__init__.py
+++ pinterest_p/__init__.py
@@ -1,9 +1,20 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from flask_bcrypt import Bcrypt
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+senha = os.getenv("SENHA")
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///master.db"
+app.config["SECRET_KEY"] = senha
 
 database = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager= LoginManager(app)
+login_manager.login_view = "homepage"
 
 from pinterest_p import routes
