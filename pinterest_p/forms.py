81739028b52cd24ca264be2cad297aa4a322db9e
BraulioHermanson <brauliohcampos0@outlook.com>
2023-09-19 10:27:40 -0300

    commit

--- pinterest_p/forms.py
+++ pinterest_p/forms.py
@@ -1,1 +1,22 @@
 # cria os formularios do site
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Email, EqualTo, Length, ValidationError
+from pinterest_p.models import Usuario
+
+class FormLogin():
+    email = StringField("E-mail", validators=[DataRequired(), Email()])
+    senha = PasswordField("Senha", validators=[DataRequired()])
+    botao_confirmacao = SubmitField("Fazer Login")
+
+class FormCriarConta():
+    email = StringField("E-mail", validators=[DataRequired(), Email()])
+    username = StringField("Nome de usuario", validators=[DataRequired()])
+    senha = PasswordField("Senha", validators=[DataRequired(), Length(6,20)])
+    confirmacao_senha = PasswordField("Confirmação de senha", validators=[DataRequired(), EqualTo("senha")])
+    botao_confirmacao = SubmitField("Criar Conta")
+
+    def validate_email(self, email):
+        usuario = Usuario.query.filter_by(email = email.data).first()
+        if usuario:
+            return ValidationError("E-mail já cadastrado, faça login para continuar.")
