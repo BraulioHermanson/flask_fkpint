ceeff4863927f57d3b4b0ca60a0d919e6d615f53
BraulioHermanson <brauliohcampos0@outlook.com>
2023-09-25 14:16:26 -0300

    commit

--- pinterest_p/forms.py
+++ pinterest_p/forms.py
@@ -1,8 +1,9 @@
-# cria os formularios do site
+# criar os formularios do nosso site
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
+from wtforms import StringField, PasswordField, SubmitField, FileField
 from wtforms.validators import DataRequired, Email, EqualTo, Length, ValidationError
 from pinterest_p.models import Usuario
+
 
 class FormLogin(FlaskForm):
     email = StringField("E-mail", validators=[DataRequired(), Email()])
@@ -16,16 +17,17 @@ class FormLogin(FlaskForm):
 
 class FormCriarConta(FlaskForm):
     email = StringField("E-mail", validators=[DataRequired(), Email()])
-    username = StringField("Nome de usuario", validators=[DataRequired()])
-    senha = PasswordField("Senha", validators=[DataRequired(), Length(6,20)])
-    confirmacao_senha = PasswordField("Confirmação de senha", validators=[DataRequired(), EqualTo("senha")])
+    username = StringField("Nome de usuário", validators=[DataRequired()])
+    senha = PasswordField("Senha", validators=[DataRequired(), Length(6, 20)])
+    confirmacao_senha = PasswordField("Confirmação de Senha", validators=[DataRequired(), EqualTo("senha")])
     botao_confirmacao = SubmitField("Criar Conta")
 
     def validate_email(self, email):
-        usuario = Usuario.query.filter_by(email = email.data).first()
+        usuario = Usuario.query.filter_by(email=email.data).first()
         if usuario:
-            return ValidationError("E-mail já cadastrado, faça login para continuar.")
-        
-# class FormFoto(FlaskForm):
-#     foto = FileField("Foto", validators=[DataRequired()])
-#     botao_confirmacao = SubmitField("Enviar")
+            raise ValidationError("E-mail já cadastrado, faça login para continuar")
+
+
+class FormFoto(FlaskForm):
+    foto = FileField("Foto", validators=[DataRequired()])
+    botao_confirmacao = SubmitField("Enviar")
--- pinterest_p/routes.py
+++ pinterest_p/routes.py
@@ -1,35 +1,38 @@
-# criar rotas do site (links do site)
+# criar as rotas do nosso site (os links)
 from flask import render_template, url_for, redirect
-from pinterest_p import app, database, bcrypt
-from flask_login import login_required, login_user, logout_user
-from pinterest_p.forms import FormLogin, FormCriarConta
+from pinterest_p  import app, database, bcrypt
 from pinterest_p.models import Usuario, Foto
+from flask_login import login_required, login_user, logout_user, current_user
+from pinterest_p.forms import FormLogin, FormCriarConta, FormFoto
+from werkzeug.utils import secure_filename
+import os
 
-@app.route("/", methods = ["GET", "POST"])
+@app.route("/", methods=["GET", "POST"])
 def homepage():
     form_login = FormLogin()
     if form_login.validate_on_submit():
         usuario = Usuario.query.filter_by(email=form_login.email.data).first()
         if usuario and bcrypt.check_password_hash(usuario.senha, form_login.senha.data):
             login_user(usuario)
-            return redirect(url_for("perfil",usuario = usuario.username))
-    return render_template("homepage.html", form = form_login)
+            return redirect(url_for("perfil", id_usuario=usuario.id))
+    return render_template("homepage.html", form=form_login)
 
-@app.route("/criarconta", methods = ["GET", "POST"])
-def criarconta():
+
+@app.route("/criarconta", methods=["GET", "POST"])
+def criar_conta():
     form_criarconta = FormCriarConta()
     if form_criarconta.validate_on_submit():
         senha = bcrypt.generate_password_hash(form_criarconta.senha.data)
         usuario = Usuario(username=form_criarconta.username.data,
-                           email = form_criarconta.email.data,
-                             senha =senha)
+                          senha=senha, email=form_criarconta.email.data)
         database.session.add(usuario)
         database.session.commit()
-        login_user(usuario, remember = True)
-        return redirect(url_for("perfil",usuario = usuario.username))
-    return render_template("criarconta.html", form = form_criarconta)
+        login_user(usuario, remember=True)
+        return redirect(url_for("perfil", id_usuario=usuario.id))
+    return render_template("criarconta.html", form=form_criarconta)
 
-@app.route("/perfil/<usuario>", methods=["GET", "POST"])
+
+@app.route("/perfil/<id_usuario>", methods=["GET", "POST"])
 @login_required
 def perfil(id_usuario):
     if int(id_usuario) == int(current_user.id):
@@ -58,3 +61,10 @@ def perfil(id_usuario):
 def logout():
     logout_user()
     return redirect(url_for("homepage"))
+
+
+@app.route("/feed")
+@login_required
+def feed():
+    fotos = Foto.query.order_by(Foto.data_criacao.desc()).all()
+    return render_template("feed.html", fotos=fotos)
